fd8e121d3f30cdf818ba40bbbe344f3dfbc1caa3
Michael1182mitnick <56063336+Michael1182mitnick@users.noreply.github.com>
2024-07-24 22:24:46 -0800

    Create test-repo35.py

---
+++ test-repo35.py
@@ -0,0 +1,10 @@
+# # Write a Python program to sort a list of numbers in descending order.
+
+list_number = [5, 3, 6, 9, 1]
+
+# using .sort method
+list_number.sort(reverse=True)
+
+# using for loop ascending order
+for number in list_number:
+    print(number)
